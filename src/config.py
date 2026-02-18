e62707a93effb4cfe2322f79d10fffe599942390
Bipin C <vu3bpn@gmail.com>
2026-02-18 12:01:44 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
 import os
+from pathlib import Path
 
-input_laz_dir = ""
+#%% configurations for data and input/output directories
+data_dir = os.path.join(Path.cwd().parent,"data")
+input_dir = os.path.join(data_dir,"input")
+input_laz_dir = os.path.join(input_dir,"laz")
+
+
+#%% Input files
+data_url_list = os.path.join(input_dir,"data_links.txt")
