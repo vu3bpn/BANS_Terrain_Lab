2d7517a7d5fdc742f4042db767250a8578e8bcaf
Bipin C <vu3bpn@gmail.com>
2026-03-03 18:01:40 +0530

    Auto commit  Tue Mar  3 18:01:40 2026

--- src/misc_subset_data.py
+++ src/misc_subset_data.py
@@ -3,7 +3,7 @@ import itertools
 from config import *
 
 if __name__ == "__main__":
-    las_input_filenames = list(Path(input_laz_dir).rglob("*.laz"))  
+    las_input_filenames = list(Path(input_laz_dir).rglob("*.laz"))[:1]  
     for las_file in las_input_filenames:    
         print(las_file)
         las_file_p = laspy.open(las_file)
@@ -27,10 +27,11 @@ if __name__ == "__main__":
         log(f"X range : {x_min} - {x_max} ,Y range : {y_min} - {y_max}") 
         x_window = int((x_max-x_min)/subset_n)+1
         y_window = int((y_max-y_min)/subset_n)+1
+        selected_points = []
         for x_0,y_0 in itertools.product(range(subset_n),range(subset_n)):
             out_filename = os.path.split(las_file)[-1].strip(".laz")+f"subset_{x_0}_{y_0}.laz"
             out_file_path = os.path.join(debug_subset_dir,out_filename)
-            dest_las_file = laspy.open(out_file_path,mode='w',header=las_file_p.header)
+            #dest_las_file = laspy.open(out_file_path,mode='w',header=las_file_p.header)
             
             for points in las_file_p.chunk_iterator(chunk_size):
                 #points_dict = {x:list(points[x]) for x in variables}
@@ -39,6 +40,9 @@ if __name__ == "__main__":
                 x,y = points.x.copy(),points.y.copy()
                 mask = (x >= x_min+ x_0*x_window) & (x < (x_min+(x_0+1)*x_window)) & \
                         (y >= y_min + y_0*y_window) & (y < (y_min+(y_0+1)*y_window))
-                dest_las_file.write_points(points[mask])
-            dest_las_file.close()
+                selected_points.extend(points[mask].copy())
+            #dest_las_file.write_points()
+            #dest_las_file = laspy.LasData(las_file_p.header)
+            #dest_las_file.write(out_file_path)
+            #log(f"generated file {out_file_path}")
 
