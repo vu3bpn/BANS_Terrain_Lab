aa315ed4c7375b64bc8af30ea05690100ff082db
Bipin <vu3bpn@gmail.com>
2026-03-13 09:02:59 +0530

    Update geoai.tex and test_knn_filter.py

--- src/test_knn_filter.py
+++ src/test_knn_filter.py
@@ -15,17 +15,20 @@ from config import *
 from misc_utilities import *
 
 
-def knn_fill(df):
+def knn_fill_t(df):
     ground_df = df.query("classification==2")
-    knn = NearestNeighbors(n_neighbors=4)
+    knn = NearestNeighbors(n_neighbors=5)
     ground_points = np.array(ground_df[["X","Y","Z"]])
     knn.fit(ground_points)
     def get_z(row):
         if row['classification'] == 2:
             return row['Z']
-        dist,index = knn.kneighbors(np.array([row[["X","Y","Z"]]]))
+        index = knn.kneighbors(np.array([row[["X","Y","Z"]]]),
+                                    return_distance=False)
         nearest_ground_vectors = ground_points[index]
-        mean_z = np.mean(nearest_ground_vectors[:,2],dtype='int32')
+        #zvals = [x[2] for x in nearest_ground_vectors]
+        mean_z = np.mean(nearest_ground_vectors[:,:,2],dtype='int32')
+        print(mean_z)
         return mean_z
     df["Z"] =  df.apply(get_z, axis=1)
     return df
@@ -46,9 +49,18 @@ def df_to_las(df,header,out_file_path):
                 writer.write_points(record)
 
 
-
-
 if __name__ == "__main__":
+    test_df = pandas.DataFrame({"X":range(50),"Y":range(50),"Z":range(50),"classification":2*np.ones(50)})
+    test_df['Y'] += 50
+    test_df['Z'] += 150
+    classification = list(test_df['classification'])
+    classification[20:30] = [0]*10
+    test_df['classification'] = classification #test_df['classification']*(test_df['X']!=25)
+    filled_df = knn_fill_t(test_df)
+    
+    
+
+if __name__ == "__main1__":
     data_info_df = pandas.read_csv(data_info_file)
     filename_paths = {os.path.split(x)[-1]:x for x in data_info_df['filename']}
     for las_file in las_vect_dict:
@@ -61,16 +73,40 @@ if __name__ == "__main__":
         gdf = geopandas.read_file(shapefile_path)
         for row1 in gdf.iterrows():
             id = row1[1]['id']
-            geometry = row1[1].geometry
-            dtm_record,header = subset_with_geom(dtm_file_path,geometry)
-            dsm_record,dsm_header = subset_with_geom(las_file_path,geometry)
-            df = pandas.DataFrame(dtm_record.array)
-            filled_df = knn_fill(df)
             
             dtm_debug_file = os.path.join(debug_dir,f"dtm_debug_{las_file_name}_id_{id}.las")
             dsm_debug_file = os.path.join(debug_dir,f"dsm_debug_{las_file_name}_id_{id}.las")
             terrain_debug_file = os.path.join(debug_dir,f"terrain_debug_{las_file_name}_id_{id}.las")
+            
+            dtm_debug_csv_file = os.path.join(debug_dir,f"dtm_debug_{las_file_name}_id_{id}.csv")
+            dsm_debug_csv_file = os.path.join(debug_dir,f"dsm_debug_{las_file_name}_id_{id}.csv")
+            terrain_debug_csv_file = os.path.join(debug_dir,f"terrain_debug_{las_file_name}_id_{id}.csv")
+            
+            
+            geometry = row1[1].geometry
+            dtm_record,dtm_header = subset_with_geom(dtm_file_path,geometry)            
+            df = pandas.DataFrame(dtm_record.array)            
+            df_to_las(df,dtm_header,terrain_debug_file)
+            df.to_csv(terrain_debug_csv_file)
+            
+            
+            filled_df = knn_fill_t(df)            
             df_to_las(filled_df,dtm_header,dtm_debug_file)
-            df_to_las(df,dtm_header,dsm_debug_file)
+            filled_df.to_csv(dtm_debug_csv_file)
+
+            dsm_record,dsm_header = subset_with_geom(las_file_path,geometry)
+            dsm_df = pandas.DataFrame(dsm_record.array)
+            dsm_df.to_csv(dsm_debug_csv_file)
+            df_to_las(dsm_df,dsm_header,dsm_debug_file)
+
+            #df_to_las(df,header,terrain_debug_file)
+            #df_to_las(dsm_df,header,dsm_debug_file)
+            
+            
+            
+            #filled_df.to_csv(dtm_debug_csv_file)
+            #df.to_csv(terrain_debug_file)
+            #dsm_df.to_csv(dsm_debug_csv_file)
+            
             
 
